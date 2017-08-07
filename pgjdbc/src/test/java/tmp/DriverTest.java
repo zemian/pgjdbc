d5827a32b8c5d2f2b658975e3d750164059a39d6
Zemian Deng <zemian.deng@bnymellon.com>
2017-08-07 00:48:45 -0400

    zemian: add driver tests

--- pgjdbc/src/test/java/tmp/DriverTest.java
+++ pgjdbc/src/test/java/tmp/DriverTest.java
@@ -2,13 +2,10 @@ package tmp;
 
 import org.junit.Test;
 import org.postgresql.Driver;
+import org.postgresql.PGProperty;
 
-import java.lang.reflect.Field;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Types;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.DriverManager;
 import java.util.Properties;
 
 /**
@@ -18,49 +15,30 @@ public class DriverTest {
 
     @Test
     public void test() throws Exception {
-        Driver driver = new Driver();
         String url = "jdbc:postgresql://localhost/test";
-        try(Connection conn = driver.connect(url, null)) {
+        try(Connection conn = DriverManager.getConnection(url)) {
             System.out.println(conn);
         }
     }
 
-    public static Map<Integer, String> SQL_TYPE_NAMES = getSqlTypeNames();
-    public static Map<Integer, String> getSqlTypeNames() {
-        Map<Integer, String> result = new HashMap<>();
-        try {
-            Field[] fields = Types.class.getFields();
-            for (Field field : fields) {
-                Integer value = (Integer)field.get(null);
-                String name = field.getName();
-                result.put(value, name);
-            }
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Failed to get sql type names", e);
+    @Test
+    public void testConnProps() throws Exception {
+        String url = "jdbc:postgresql://localhost/test";
+        Properties info = new Properties();
+        info.setProperty("assumeMinServerVersion", "9.0");
+        try(Connection conn = DriverManager.getConnection(url, info)) {
+            System.out.println(conn);
         }
-        return result;
     }
 
     @Test
-    public void testDataTypes() throws Exception {
+    public void testDriverConnect() throws Exception {
         Driver driver = new Driver();
         String url = "jdbc:postgresql://localhost/test";
-        try(Connection conn = driver.connect(url, null)) {
-            ResultSet rs = conn.getMetaData().getTypeInfo();
-            int count = 0;
-            while(rs.next()) {
-                String name = rs.getString("TYPE_NAME");
-                int type = rs.getInt("DATA_TYPE");
-                String typeName = SQL_TYPE_NAMES.get(type);
-
-                System.out.println(
-                        "TYPE_NAME=" + name +
-                                ", DATA_TYPE=" + type +
-                                ", SQL_TYPE=" + typeName);
-                count ++;
-            }
-            rs.close();
-            System.out.println(count + " found.");
+        Properties info = new Properties();
+        info.setProperty("assumeMinServerVersion", "9.0");
+        try(Connection conn = driver.connect(url, info)) {
+            System.out.println(conn);
         }
     }
 }
--- pgjdbc/src/test/java/tmp/LoginTimeoutTest.java
+++ pgjdbc/src/test/java/tmp/LoginTimeoutTest.java
@@ -2,12 +2,17 @@ package tmp;
 
 import org.junit.Test;
 import org.postgresql.Driver;
+import org.postgresql.PGProperty;
 import org.postgresql.test.TestUtil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 /**
+ * Issue: https://github.com/pgjdbc/pgjdbc/issues/879
+ * PR: https://github.com/pgjdbc/pgjdbc/pull/900
+ *
  * Created by xbbj5x6 on 8/4/2017.
  */
 public class LoginTimeoutTest {
@@ -49,5 +54,36 @@ public class LoginTimeoutTest {
         try(Connection conn = driver.connect(url, null)) {
             System.out.println(conn);
         }
+    }
+
+    @Test
+    public void testDriverManager3ConfusingCase() throws Exception {
+        DriverManager.setLoginTimeout(5);
+        String url = "jdbc:postgresql://localhost/test";
+        Properties info = new Properties();
+        info.setProperty("loginTimeout", "13");
+        // Due to way info props is loaded as Properties#defauts, the PGProperty.isPresent will return false!
+        // Hence, this test case will use DriverManager.loginTimeout value instead of info props!
+        // See testPGPropertyIsPresent()
+
+        Driver driver = new Driver();
+        try(Connection conn = driver.connect(url, info)) {
+            System.out.println(conn);
+        }
+    }
+
+    @Test
+    public void testPGPropertyIsPresent() throws Exception {
+        // PGProperty.isPresent can be very misleading!!!
+        // The properties info set as object will only be use as Properties#defaults, which is not the same
+        // as populating the properties!
+        Properties info = new Properties();
+        info.setProperty("loginTimeout", "11");
+        System.out.println(PGProperty.LOGIN_TIMEOUT.isPresent(info));
+        System.out.println(PGProperty.LOGIN_TIMEOUT.get(info));
+
+        Properties info2 = new Properties(info);
+        System.out.println(PGProperty.LOGIN_TIMEOUT.isPresent(info2));
+        System.out.println(PGProperty.LOGIN_TIMEOUT.get(info2));
     }
 }
