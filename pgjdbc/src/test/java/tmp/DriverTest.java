be60b09e1c6fadb188ddcfdfe1fef7f2c16a750e
Zemian Deng <zemian.deng@bnymellon.com>
2017-08-08 23:09:03 -0400

    zemian: add notes on how PG messages are sent/receive through conn

--- pgjdbc/src/test/java/tmp/DriverTest.java
+++ pgjdbc/src/test/java/tmp/DriverTest.java
@@ -15,10 +15,17 @@ import java.util.Properties;
  * Created by xbbj5x6 on 8/4/2017.
  */
 public class DriverTest {
-
     @Test
     public void test() throws Exception {
-        String url = "jdbc:postgresql://localhost/test";
+        String url = "jdbc:postgresql://localhost/test?user=test";
+        try(Connection conn = DriverManager.getConnection(url)) {
+            System.out.println(conn);
+        }
+    }
+
+    @Test
+    public void testPassword() throws Exception {
+        String url = "jdbc:postgresql://localhost/test?user=test&password=password";
         try(Connection conn = DriverManager.getConnection(url)) {
             System.out.println(conn);
         }
@@ -28,6 +35,7 @@ public class DriverTest {
     public void testConnProps() throws Exception {
         String url = "jdbc:postgresql://localhost/test";
         Properties info = new Properties();
+        info.setProperty("user", "test");
         info.setProperty("assumeMinServerVersion", "9.0");
         try(Connection conn = DriverManager.getConnection(url, info)) {
             System.out.println(conn);
@@ -39,6 +47,7 @@ public class DriverTest {
         Driver driver = new Driver();
         String url = "jdbc:postgresql://localhost/test";
         Properties info = new Properties();
+        info.setProperty("user", "test");
         info.setProperty("assumeMinServerVersion", "9.0");
         try(Connection conn = driver.connect(url, info)) {
             System.out.println(conn);
