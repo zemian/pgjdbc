de8a5021979a62bf1c637d6d496b62d743f4c076
Zemian Deng <zemian.deng@bnymellon.com>
2017-08-11 20:53:36 -0400

    Add DataTypeTest.java

--- pgjdbc/src/test/java/zemian/DataTypeTest.java
+++ pgjdbc/src/test/java/zemian/DataTypeTest.java
@@ -34,7 +34,7 @@ public class DataTypeTest {
     @Test
     public void testDataTypes() throws Exception {
         Driver driver = new Driver();
-        String url = "jdbc:postgresql://localhost/test";
+        String url = "jdbc:postgresql://localhost/test?user=test";
         try(Connection conn = driver.connect(url, null)) {
             ResultSet rs = conn.getMetaData().getTypeInfo();
             int count = 0;
@@ -45,12 +45,101 @@ public class DataTypeTest {
 
                 System.out.println(
                         "TYPE_NAME=" + name +
-                                ", DATA_TYPE=" + type +
-                                ", SQL_TYPE=" + typeName);
+                        ", DATA_TYPE=" + type +
+                        ", SQL_TYPE=" + typeName);
                 count ++;
             }
-            rs.close();
             System.out.println(count + " found.");
+            rs.close();
+        }
+    }
+
+    @Test
+    public void tetsGetFunctions() throws Exception {
+        Driver driver = new Driver();
+        String url = "jdbc:postgresql://localhost/test?user=test";
+        try(Connection conn = driver.connect(url, null)) {
+            ResultSet rs = conn.getMetaData().getFunctions(null, null, null);
+            int maxCols = rs.getMetaData().getColumnCount();
+            for (int i =1; i <= maxCols; i++) {
+                System.out.println("Idx#" + i + " " + rs.getMetaData().getColumnName(i));
+            }
+
+//            int count = 0;
+//            while(rs.next()) {
+////                String cat = rs.getString("FUNCTION_CAT");
+////                String schem = rs.getString("FUNCTION_SCHEM");
+////                String name = rs.getString("FUNCTION_NAME");
+////                String remarks = rs.getString("REMARKS");
+////                int type = rs.getShort("FUNCTION_TYPE");
+//
+//                String cat = rs.getString(1);
+//                String schem = rs.getString(2);
+//                String name = rs.getString(3);
+//                String remarks = rs.getString(4);
+//                int type = rs.getShort(5);
+//
+//                System.out.println(
+//                        "FUNCTION_CAT=" + cat +
+//                        ", FUNCTION_SCHEM=" + schem +
+//                        ", FUNCTION_NAME=" + name +
+//                        ", REMARKS=" + remarks +
+//                        ", FUNCTION_TYPE=" + type);
+//                count ++;
+//            }
+//            System.out.println(count + " found.");
+            rs.close();
+        }
+    }
+
+    @Test
+    public void testGetProcedures() throws Exception {
+        Driver driver = new Driver();
+        String url = "jdbc:postgresql://localhost/test?user=test";
+        try(Connection conn = driver.connect(url, null)) {
+            ResultSet rs = conn.getMetaData().getProcedures(null, null, null);
+            int maxCols = rs.getMetaData().getColumnCount();
+            for (int i =1; i <= maxCols; i++) {
+                System.out.println("Idx#" + i + " " + rs.getMetaData().getColumnName(i));
+            }
+
+//            int count = 0;
+//            while(rs.next()) {
+////                String cat = rs.getString("FUNCTION_CAT");
+////                String schem = rs.getString("FUNCTION_SCHEM");
+////                String name = rs.getString("FUNCTION_NAME");
+////                String remarks = rs.getString("REMARKS");
+////                int type = rs.getShort("FUNCTION_TYPE");
+//
+//                String cat = rs.getString(1);
+//                String schem = rs.getString(2);
+//                String name = rs.getString(3);
+//                String remarks = rs.getString(4);
+//                int type = rs.getShort(5);
+//
+//                System.out.println(
+//                        "FUNCTION_CAT=" + cat +
+//                        ", FUNCTION_SCHEM=" + schem +
+//                        ", FUNCTION_NAME=" + name +
+//                        ", REMARKS=" + remarks +
+//                        ", FUNCTION_TYPE=" + type);
+//                count ++;
+//            }
+//            System.out.println(count + " found.");
+            rs.close();
+        }
+    }
+
+    @Test
+    public void tetsGetFunctions2() throws Exception {
+        Driver driver = new Driver();
+        String url = "jdbc:postgresql://localhost/test?user=test";
+        try(Connection conn = driver.connect(url, null)) {
+            ResultSet rs = conn.getMetaData().getFunctions(null, null, null);
+            while (rs.next() ) {
+                System.out.println(rs.getString("FUNCTION_NAME"));
+            }
+            rs.close();
         }
     }
 }
